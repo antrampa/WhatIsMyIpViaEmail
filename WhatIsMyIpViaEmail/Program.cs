14464c20fd157837037b81780cb71990a2916a1e
Antonis Zalonis <a.zalonis@sigmasolutions.gr>
2016-09-23 14:32:23 +0300

    Send e-mail

--- WhatIsMyIpViaEmail/Program.cs
+++ WhatIsMyIpViaEmail/Program.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+
 
 namespace WhatIsMyIpViaEmail
 {
@@ -36,6 +40,15 @@ namespace WhatIsMyIpViaEmail
                         stringhtml += counter + ". " + aTag.InnerHtml + " - " + aTag.Attributes["href"].Value + "\t" + "<br />"; //ip
                         if(aTag.Attributes["href"].Value.StartsWith("//whatismyipaddress.com/ip/"))
                         {
+                            string currentIP = aTag.InnerHtml;// Attributes["href"].Value;
+                            if(currentIP != Settings.Default.MyIp)
+                            {
+                                Settings.Default.MyIp = currentIP;
+                                Settings.Default.Save();
+                                //Send e-mail
+                                SendEmail(currentIP);
+                            }
+
                             Console.WriteLine("MyIpIs");
                             Console.WriteLine(aTag.InnerHtml);
                         }
@@ -47,42 +60,60 @@ namespace WhatIsMyIpViaEmail
 
             Console.WriteLine(url);
             //Console.WriteLine(stringhtml);
-
-
-            //string url = "http://www.whatsmyip.org/";
-            //HtmlWeb web = new HtmlWeb();
-            //HtmlDocument doc = web.Load(url);
-            //var request = (HttpWebRequest)WebRequest.Create(url);
-            //request.UserAgent = @"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5";
-
-            //string stringhtml = string.Empty;
-            //var aTags = doc.DocumentNode.SelectNodes("//span");
-            //int counter = 1;
-            //if (aTags != null)
-            //{
-            //    foreach (var aTag in aTags)
-            //    {
-            //        stringhtml += counter + ". " + aTag.InnerHtml + " - " + aTag.Attributes["id"].Value + "\t" + "<br />"; //ip
-            //        counter++;
-            //    }
-            //}
-
-            //using (var client = new WebClient())
-            //{
-            //    string html = client.DownloadString("http://www.whatismyip.com");
-            //    // TODO: do something with the downloaded result from the remote
-            //    // web site
-
-            //    var doc = new HtmlDocument();
-            //    doc.LoadHtml(html);
-
-            //    var text = doc.DocumentNode.Descendants()
-            //                  .Where(x => x.NodeType == HtmlNodeType.Text && x.InnerText.Trim().Length > 0)
-            //                  .Select(x => x.InnerText.Trim());
-
-            //    //var web = new HtmlWeb();
-            //    //var doc = web.Load(url);
-            //}
         }
+
+        private static void SendEmail(string currentIpAddress)
+        {
+            try
+            {
+                string emailFrom = Settings.Default.EmailAddressFrom;
+                string emailTo = Settings.Default.EmailAddressToSend;
+                string fromName = Settings.Default.FromName;
+                string toName = Settings.Default.ToName;
+                string username = Settings.Default.SendEmailUsername; //"username";
+                string password = Settings.Default.SendEmailPassword; //"password";
+                string smtp = Settings.Default.SendEmailSMTP; //"smtp.gmail.com";
+                int port = Settings.Default.SendEmailSMTPPort; //587;
+
+                SmtpClient mySmtpClient = new SmtpClient(smtp, port);
+                mySmtpClient.EnableSsl = true;
+                // set smtp-client with basicAuthentication
+                mySmtpClient.UseDefaultCredentials = false;
+                System.Net.NetworkCredential basicAuthenticationInfo = new
+                   System.Net.NetworkCredential(username, password);
+                mySmtpClient.Credentials = basicAuthenticationInfo;
+
+                // add from,to mailaddresses
+                MailAddress from = new MailAddress(emailFrom, fromName);
+                MailAddress to = new MailAddress(emailTo, toName);
+                MailMessage myMail = new System.Net.Mail.MailMessage(from, to);
+
+                //// add ReplyTo
+                //MailAddress replyto = new MailAddress(email);
+                //myMail.ReplyToList.Add(replyto);
+
+                // set subject and encoding
+                myMail.Subject = "Your IP Address has changed";
+                myMail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                // set body-message and encoding
+                myMail.Body = "<b>Your IP address now is:</b><br><b>"+ currentIpAddress + "</b>.";
+                myMail.BodyEncoding = System.Text.Encoding.UTF8;
+                // text or html
+                myMail.IsBodyHtml = true;
+
+                mySmtpClient.Send(myMail);
+            }
+            catch (SmtpException ex)
+            {
+                throw new ApplicationException("SmtpException has occured: " + ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("SendEmail: " + ex.Message);
+            }
+
+        }
+
     }
 }
